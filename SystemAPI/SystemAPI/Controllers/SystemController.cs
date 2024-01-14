9a8c231c26f084eef2ac6e080b2d25e21969661d
Nikolay Shtipkov <nikolayshtipkov@gmail.com>
2024-01-14 20:47:32 +0200

    add controllers

---
+++ SystemAPI/SystemAPI/Controllers/SystemController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SystemAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SystemController : ControllerBase
+    {
+    }
+}
