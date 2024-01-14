4366d91eb1cc750642ca030c3e032692f7673df7
Nikolay Shtipkov <nikolayshtipkov@gmail.com>
2024-01-14 16:57:31 +0200

    add necessary packages

--- SystemAPI/System.Data/Entities/Entity.cs
+++ SystemAPI/System.Data/Entities/Entity.cs
@@ -4,8 +4,6 @@ namespace System.Data.Entities
 {
     public class Entity
     {
-        [Key]
-        public Guid Id { get; set; }
         [Required]
         public string Name { get; set; }
     }
